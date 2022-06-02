3c14951c9cc272d3c00efa94ffcc2273a749a390
himdol <ihimchan746@gmail.com>
2022-06-03 08:06:33 +0900

    [제 2장] JUnit5 코드 일부 작성 일부 미흡

---
+++ src/main/java/com/limvo/front/web/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.limvo.front.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/limvo/front/web/HelloRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.limvo.front.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloRestController.class)
+public class HelloRestControllerTest {
+
+
+//    @Autowired
+    private MockMvc mockMvc;
+
+    public HelloRestControllerTest(MockMvc mockMvc){
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void returnTextHello() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
