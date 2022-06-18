6b007c65e723e210c86298b3d1a5edb9bfdafd2e
himdol <ihimchan746@gmail.com>
2022-06-18 19:04:51 +0900

    [제 5장 완료] 구글 로그인 기능 일부 추가

---
+++ src/main/java/com/limvo/front/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.limvo.front.config.auth.dto;
+
+import com.limvo.front.web.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
