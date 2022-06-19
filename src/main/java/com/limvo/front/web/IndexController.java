7c1a652d9921e5d5c10b9de2f0a3c62812036875
himdol <ihimchan746@gmail.com>
2022-06-19 12:50:51 +0900

    [제5장] 구글 로그인 및 회원가입 권한관리 구현

--- src/main/java/com/limvo/front/web/IndexController.java
+++ src/main/java/com/limvo/front/web/IndexController.java
@@ -1,24 +1,37 @@
 package com.limvo.front.web;
 
+import com.limvo.front.config.auth.dto.SessionUser;
 import com.limvo.front.web.dto.PostsResponseDto;
 import com.limvo.front.web.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    public IndexController(PostsService postsService) {
+    public IndexController(PostsService postsService, HttpSession httpSession) {
         this.postsService = postsService;
+        this.httpSession = httpSession;
     }
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
