39e9311825cbb7cbbcb92c60c5d140530805fa92
himdol <ihimchan746@gmail.com>
2022-07-10 11:29:04 +0900

    [제 5장] Google login 연동 성공

--- src/main/java/com/limvo/front/web/IndexController.java
+++ src/main/java/com/limvo/front/web/IndexController.java
@@ -1,22 +1,34 @@
 package com.limvo.front.web;
 
+import com.limvo.front.config.auth.dto.SessionUser;
 import com.limvo.front.web.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
+
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    public IndexController(PostsService postsService) {
+    public IndexController(PostsService postsService, HttpSession httpSession) {
         this.postsService = postsService;
+        this.httpSession = httpSession;
     }
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+
+        if(!Objects.isNull(sessionUser)){
+            model.addAttribute("userName", sessionUser.getName());
+        }
+
         return "index";
     }
 
