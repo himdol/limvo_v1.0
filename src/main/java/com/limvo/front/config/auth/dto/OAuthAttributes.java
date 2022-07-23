dc9657e8eaea6fe438505bfe773873b35e2498bf
himdol <ihimchan746@gmail.com>
2022-07-23 12:17:22 +0900

    [제 5장] 구글, 네이버 연동 성공

--- src/main/java/com/limvo/front/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/limvo/front/config/auth/dto/OAuthAttributes.java
@@ -1,5 +1,6 @@
 package com.limvo.front.config.auth.dto;
 
+import com.limvo.front.web.domain.CommonConstants;
 import com.limvo.front.web.domain.user.Role;
 import com.limvo.front.web.domain.user.User;
 import lombok.Builder;
@@ -25,7 +26,11 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-        return ofGoogle(userNameAttributeName, attributes);
+        if(CommonConstants.NAVER.equals(registrationId)){
+            return ofNaver("id", attributes);
+        } else {
+            return ofGoogle(userNameAttributeName, attributes);
+        }
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -34,6 +39,17 @@ public class OAuthAttributes {
                 .email(String.valueOf(attributes.get("email")))
                 .picture(String.valueOf(attributes.get("picture")))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
---
+++ src/main/java/com/limvo/front/web/domain/CommonConstants.java
@@ -0,0 +1,5 @@
+package com.limvo.front.web.domain;
+
+public class CommonConstants {
+    public final static String NAVER = "naver";
+}
